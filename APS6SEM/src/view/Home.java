93fdeef3420aa2b4fa22ba120850cabf57b301c5
cassia.rezende <cassia.rezende@agrotools.com.br>
2020-10-20 22:49:28 -0300

    criação das telas

--- APS6SEM/src/view/Home.java
+++ APS6SEM/src/view/Home.java
@@ -7,7 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class TelaHome extends JFrame {
+public class Home extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +18,7 @@ public class TelaHome extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaHome frame = new TelaHome();
+					Home frame = new Home();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class TelaHome extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TelaHome() {
+	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
